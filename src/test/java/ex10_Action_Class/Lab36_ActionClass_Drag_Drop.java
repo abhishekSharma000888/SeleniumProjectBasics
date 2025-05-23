6f2a6fe3b3335c03a5e0ea821f723cd274873cde
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-05-23 12:56:15 +0530

    added actionClass

---
+++ src/test/java/ex10_Action_Class/Lab36_ActionClass_Drag_Drop.java
@@ -0,0 +1,30 @@
+package ex10_Action_Class;
+
+import ex11_File_Upload.CommonToAll;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class Lab36_ActionClass_Drag_Drop extends CommonToAll {
+
+    @Test
+    public void test_actions_p5() {
+
+        driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+        driver.manage().window().maximize();
+
+        Actions actions = new Actions(driver);
+
+        // Source , Destination
+        //  from and to
+        WebElement from = driver.findElement(By.id("column-a"));
+        WebElement to = driver.findElement(By.id("column-b"));
+
+        actions.dragAndDrop(from,to).build().perform();
+
+
+
+
+    }
+}
